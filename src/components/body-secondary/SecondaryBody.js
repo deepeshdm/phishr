8b55e34881809e2bf83ba01a4ff8bdd4f1618f67
deepeshdm <deepeshmhatre133@gmail.com>
2023-01-07 19:32:17 +0530

    added responsive

--- src/components/body-secondary/SecondaryBody.js
+++ src/components/body-secondary/SecondaryBody.js
@@ -6,12 +6,16 @@ export function SecondaryBody() {
   return (
     <>
       <div className="relative">
-        <img src={BackgroundShape} alt="BackgroundImage" className="mt-1" />
-        <p className="absolute top-28 left-[42%] text-white font-semibold text-3xl text-roboto text-center">
-          {" "}
-          How does it Work ?{" "}
+        <img src={BackgroundShape} alt="BackgroundImage" className="mt-1 max-sm:h-80 sm:max-lg:h-96" />
+        <p className="text-white font-semibold text-roboto text-center absolute max-sm:top-14 top-16 lg:top-24 text-2xl
+        min-[350px]:left-[27%] min-[400px]:left-[33%] min-[520px]:left-[38%] md:left-[40%] lg:left-[42%]
+        min-[350px]:text-xl lg:text-3xl">
+          How does it Work ?
         </p>
-        <p className="absolute top-40 text-roboto text-white font-light text-lg mx-24 text-center">
+        <p className="absolute text-roboto text-white font-light text-center 
+         max-sm:text-xs sm:text-sm md:text-base lg:text-lg max-sm:top-24 top-24 lg:top-36 max-md:mx-7 md:mx-14 lg:mx-20
+
+        ">
           Phishing is a type of online scam where criminals impersonate
           legitimate organizations via email, text message, advertisement or
           other means in order to steal sensitive information. This is usually
--- src/components/body-secondary/UrlFeatures.js
+++ src/components/body-secondary/UrlFeatures.js
@@ -5,16 +5,16 @@ export function UrlFeatures(){
 
     return(
         <>
-       <div className="flex flex-row absolute bottom-14 gap-x-10 ml-36 text-roboto">
+       <div className="flex flex-row bottom-14 gap-x-10 text-roboto xl:ml-36">
 
-       <div className=" bg-white w-96 px-8 py-4 rounded-md opacity-95">
+       <div className=" bg-white w-96 px-8 py-4 rounded-md opacity-95 border-2 border-black">
             <p className="font-bold text-gray-800 text-xl text-center"> Length Features </p>
             <p className="text-center text-gray-800 text-sm mt-2"> The length of the URL, hostname, path, and top-level domain could potentially be indicators of malicious activity. 
                 For example, a URL with a very long hostname or path could be more suspicious than a shorter one.
             </p>
         </div>
 
-        <div className=" bg-white w-96 px-8 py-4 rounded-md opacity-95">
+        <div className=" bg-white w-96 px-8 py-4 rounded-md opacity-95 border-2 border-black">
             <p className="font-bold text-gray-800 text-xl text-center"> Count Features </p>
             <p className="text-center text-sm mt-2 text-gray-800">
             The number of letters, digits, special characters, and directories in a URL could also be useful indicators. For example, 
@@ -22,7 +22,7 @@ export function UrlFeatures(){
             </p>
         </div>
 
-        <div className=" bg-white w-96 px-8 py-4 rounded-md opacity-95">
+        <div className=" bg-white w-96 px-8 py-4 rounded-md opacity-95 border-2 border-black">
             <p className="font-bold text-gray-800 text-xl text-center"> Binary Features </p>
             <p className="text-center text-sm mt-2 text-gray-800"> 
             Whether the URL uses an IP address or URL shortening could be useful indicators as well. For example, a URL that uses an 
